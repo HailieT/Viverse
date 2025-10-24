739e4f91d3306757a7e962132df2d33ca381e5b7
Ares G <user@FIH-COM23a109.fsu.edu>
2025-10-24 15:59:20 -0400

    made intro scene more functional ( added audio and made play goto scene one and added credits) - still need to make vr work

--- Assets/Scenes/TitleScreenUI/MenuController.cs
+++ Assets/Scenes/TitleScreenUI/MenuController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -30,36 +29,3 @@ public class MenuController : MonoBehaviour
         #endif
     }
 }
-=======
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class MenuController : MonoBehaviour
-{
-    // Make sure your Game Scene is added to the Build Settings
-    // and its index (e.g., 1) is correct.
-    public void PlayGame()
-    {
-        // VR games often use a specific "loading" or "game" scene
-        // which will contain the VR rig and other necessary components.
-        // Index 1 typically refers to the next scene after the Main Menu (Index 0).
-        SceneManager.LoadScene(1);
-
-        // OR, use the scene name if you prefer:
-        // SceneManager.LoadScene("YourGameSceneName");
-    }
-
-    public void ExitGame()
-    {
-        // This command is the standard way to quit a built application.
-        Application.Quit();
-
-        // This conditional block is for testing *inside* the Unity Editor only.
-        // It's a standard practice to include this when testing quit functionality.
-        #if UNITY_EDITOR
-            // This is the correct way to stop the game playing in the Editor.
-            UnityEditor.EditorApplication.isPlaying = false;
-        #endif
-    }
-}
->>>>>>> 286a0b93b0241a5f8ba89463b1e08ed5e205bf6a
